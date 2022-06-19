79e4aa2f56c56b1ba252fd4a064030f508a4eab6
reza <reza138398123@gmail.com>
2022-06-19 11:33:32 +0430

    the insert , select, update , delete is added (part 2)

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from enum import unique
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import and_
 
 # part1
 # database+ library://username:password@host:port
-engine = create_engine('sqlite:///database.db')
+engine = create_engine('sqlite:///database.db') # echo=True
 base = declarative_base()
+session = sessionmaker(bind=engine)()
 
-class student(base):
+class Student(base):
     __tablename__ = 'student'
     _id = Column('id', Integer, unique=True, primary_key=True)
     name = Column('name', String(50))
@@ -17,3 +20,36 @@ base.metadata.create_all(engine)
 
 
 # part2
+
+# select
+# students = session.query(Student).order_by(Student.name).all() # .filter(and_(Student._id == 2 , Student.name == 'reza')).order_by(Student.name)
+# for student in students:
+#     print(student._id, student.name)
+# print(students)
+
+
+# insert
+# student_1 = Student(name='mohammad1')
+# student_2 = Student(name='mohammad2')
+
+# session.add_all([student_1, student_2])
+# session.commit()
+
+
+
+# delete
+# student = session.query(Student).filter(Student._id == 6).first()
+# session.delete(student)
+# session.commit()
+
+# student = session.query(Student).filter(Student._id == 5).delete()
+# session.commit()
+
+
+# update
+# student = session.query(Student).filter(Student.name == "mohammad").update({'name': 'mohammad ali'})
+# session.commit()
+
+# student = session.query(Student).filter(Student.name == 'reza').first()
+# student.name = 'reza1'
+# session.commit()
